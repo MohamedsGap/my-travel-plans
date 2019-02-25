8d5c9dee50c3d200acb72ed21c73cf212023b049
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2019-02-26 00:14:14 +0200

    add function to handle text transform

--- js/app.js
+++ js/app.js
@@ -15,3 +15,19 @@ function moveParis(){
 londonHere.addEventListener('click', moveLondon);
 dest.addEventListener('click',move);
 parisHere.addEventListener('click', moveParis);
+
+
+let spans = document.querySelectorAll('.word span');
+spans.forEach((span, idx) => {
+	span.addEventListener('click', (e) => {
+		e.target.classList.add('active');
+	});
+	span.addEventListener('animationend', (e) => {
+		e.target.classList.remove('active');
+	});
+
+	// Initial animation
+	setTimeout(() => {
+		span.classList.add('active');
+	}, 750 * (idx+1))
+});
